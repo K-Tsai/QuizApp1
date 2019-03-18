a68323d7c96fab63cbc3da31d904102527ca643a
Kevin Tsai <k.tsai92@gmail.com>
2019-03-17 20:20:41 -0400

    Commit without Question Fix

---
+++ datastore.js
@@ -0,0 +1,87 @@
+const STORE = [
+	{
+		question: "What was the highest selling video game on PlayStation 1 that featured a main character with blond and spiky hair?",
+		answers: [
+		"Crash Bandicoot",
+		"Metal Gear Solid",
+		"Final Fantasy VII",
+		"Final Fantasy VIII"
+		],
+		correctAnswer: "Final Fantasy VII",
+		icon: 'https://res.cloudinary.com/teepublic/image/private/s--4lRhtBvj--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1446223450/production/designs/187828_1.jpg',
+		alt: 'Final Fantasy 7 icon'
+	},
+	{
+		question: "Who is the main protaganist of Nintendo's video game series, The Legend of Zelda?",
+		answers: [
+		"Mario",
+		"Link",
+		"Zelda",
+		"Kirby"
+		],
+		correctAnswer: "Link",
+		icon: 'https://vignette.wikia.nocookie.net/joke-battles/images/8/8b/Link4.png/revision/latest?cb=20160407235715',
+		alt: 'Link icon'
+	},
+	{
+		question: "Which game features an energetic young purple dragon who is known to defeat enemies by breathing fire and charging at them?",
+		answers: [
+		"Spyro",
+		"Barney",
+		"Jak and Dexter",
+		"Sly Cooper"
+		],
+		correctAnswer: "Spyro",
+		icon: 'https://pbs.twimg.com/profile_images/1062210297769984000/X2zf9SS8_400x400.jpg',
+		alt: "Spyro icon"
+	},
+	{
+		question: "Which Nintendo RPG series was known its distinctive visual style, which consisted of 2D cutout characters moving about in 3D worlds?", 
+		answers: [
+		"Super Mario World",
+		"Super Mario 64",
+		"Paper Mario",
+		"Mario Party"
+		],
+		correctAnswer: "Paper Mario",
+		icon: "https://static.tvtropes.org/pmwiki/pub/images/color_splash_paper_mario.png",
+		alt: "Paper Mario icon"
+	},
+	{
+		question: "What is the crossover fighting game developed by HAL Labratory that featured many different Nintendo franchises?",
+		answers: [
+		"Street Fighter",
+		"Super Smash Bros",
+		"Mortal Kombat",
+		"Tekken"
+		],
+		correctAnswer: "Super Smash Bros",
+		icon: "https://www.smashbros.com/assets_v2/img/top/hero05_en.jpg",
+		alt: "SUper Smash Bros icon"
+	},
+	{
+		question: "What is the name of Megaman in Japan?",
+		answers: [
+		"Rockman",
+		"Blueman",
+		"Robotman",
+		"Paul"
+		],
+		correctAnswer: "Rockman",
+		icon: "https://vignette.wikia.nocookie.net/megaman/images/0/02/RockmanKikiIppatsu.png/revision/latest?cb=20120823170644",
+		alt: "Megaman icon"
+	},
+	{
+		question: "Which video game is known as the hardest of all time?",
+		answers: [
+		"Ghosts 'n Goblins",
+		"Ninja Gaiden",
+		"Teenage Mutant Ninja Turtles",
+		"Dark Souls"
+		],
+		correctAnswer: "Ghosts 'n Goblins",
+		icon: "https://nintendowire.com/wp-content/uploads/2019/01/Ghosts-n-Goblins-Title-Screen.png",
+		alt: "Ghost 'n Goblins icon"
+	}
+];
+	
---
+++ index.js
@@ -0,0 +1,136 @@
+let questionNumber = 0;
+let score = 0;
+
+function generateQuestion() {
+	if (questionNumber < STORE.length) {
+	return ` 
+	<div class= "questionForm">
+	<h2>${STORE[questionNumber].question}</h2>
+	<form>
+		<fieldset>
+			<label class="answerOption">
+				<input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+				<span>${STORE[questionNumber].answers[0]}</span>
+			</label>
+			<label class ="answerOption">
+				<input type= "radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+				<span>${STORE[questionNumber].answers[1]}</span>
+			</label>
+			<label class ="answerOption">
+				<input type= "radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+				<span>${STORE[questionNumber].answers[2]}</span>
+			</label>
+			<label class ="answerOption">
+				<input type= "radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+				<span>${STORE[questionNumber].answers[3]}</span>
+			</label>
+			<button type="submit" class="submitButton">Submit</button>
+		</fieldset>
+	</form>
+	</div>
+	`;
+	} else {
+		renderResults();
+		restartQuiz();
+		$('.questionNumber').text(1);
+	}
+}
+
+
+function startButton() {
+	$('.quizStart').on('click', '.startButton', function (event) {
+		$('.quizStart').remove();
+		$('.questionAnswerForm').css('display', 'block');
+		$('.questionNumber').text(1);
+  });
+} 
+
+function renderQuestion() {
+	$('.questionAnswerForm').html(generateQuestion());
+}
+
+function incrementQuestion() {
+	questionNumber++;
+}
+
+function incrementScore() {
+	score++;
+}
+
+function changeScore () {
+	incrementScore();
+	$('.scores').text(score);
+}
+
+function submitAnswer() {
+		$('form').on('submit', function (event) {
+		  event.preventDefault();
+		  let selected = $('input:checked');
+		  let answer = selected.val();
+		  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+		  if (answer === correctAnswer) {
+			correctFeedback();
+			changeScore();
+			incrementQuestion();
+			} else {
+			wrongFeedback();
+			incrementQuestion();
+			}
+		});
+	  }
+
+function correctFeedback() {
+	$('.questionAnswerForm').html(`<div class = "correctFeedback"><img src = 
+	"${STORE[questionNumber].icon}" class = "correctImg"><p>The answer was correct!</p>
+	<button type=button class="nextButton">Onward!</button>
+	</div> `);
+}
+
+function wrongFeedback(){
+	$('.questionAnswerForm').html(`<div class = "wrongFeedback"><img src = 
+	"${STORE[questionNumber].icon}" class = "correctImg"><p>The answer was 
+	${STORE[questionNumber].correctAnswer}</p>
+	<button type=button class="nextButton">Onward!</button>
+	</div> `);
+}
+
+function nextButton() {
+	$('main').on('click', '.nextButton', function(event) {
+		renderQuestion();
+		submitAnswer();
+		$('.questionNumber').text(questionNumber+1);
+    //This function is responsible for handling the next button.
+    console.log('`nextButton`, ran')
+});
+}
+
+function renderResults() {
+	if (score < 7){
+		$('.questionAnswerForm').html(`<div class= "results"><h1>
+		Play again and try to get all the answers correct!</h1>
+		<p>Your score is ${score} / 7</p><button class = "restartButton">
+		Restart?</button></div>`);
+	} else {
+		$('.questionAnswerForm').html(`<div class= "results"><h1>
+		Good job you got it all correct!</h1>
+		<p>Your score is ${score} / 7</p><button class = "restartButton">
+		Restart?</button></div>`);
+	}
+}
+
+function restartQuiz(){
+	$('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+    //This function is responsible for restarting the quiz when pressing button.
+    console.log('`restartQuiz`, ran')
+}
+
+function handleQuiz() {
+	startButton();
+	renderQuestion();
+	submitAnswer();
+	nextButton();
+}
+
+$(handleQuiz);
